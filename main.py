c162483ddba2204b5ad96c4a61e6baaaa33205e4
Karter <MAKCNM.0709@gmail.com>
2022-04-02 10:23:27 +0300

    'init'

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('add')
+import aiogram
+from  aiogram import Dispatcher, executor, types, Bot
+
+bot  = Bot(token='5146638518:AAExU7G5-hFexzI2bNdPJBAC1giRbCwbMx8')
+
+dis = Dispatcher(bot)
+
+@dis.message_handler(commands='start')
+async def start(message : types.Message):
+    await message.answer('Привет')
+
+if __name__ == '__main__':
+    executor.start_polling(skip_updates=True)
