28cf1709215aef63fcc8a6e0410b9764282b7dd9
Karter <MAKCNM.0709@gmail.com>
2022-04-02 10:40:06 +0300

    'init'

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+#sqlite3------------------------------------------
+import sqlite3
+connect = sqlite3.connect('src/db/async.db')
+cursors = connect.cursor()
+
+#aiogram----------------------------------------------
 import aiogram
 from  aiogram import Dispatcher, executor, types, Bot
+from aiogram.dispatcher.filters.state import State, StatesGroup
+#---------------------------------------------------------------
 
 bot  = Bot(token='5146638518:AAExU7G5-hFexzI2bNdPJBAC1giRbCwbMx8')
 
 dis = Dispatcher(bot)
 
+class Order(StatesGroup):
+    product = State()
+    pay = State()
+
 @dis.message_handler(commands='start')
 async def start(message : types.Message):
     await message.answer('Привет')
 
+
+@dis.message_handler(lambda message : message.text == "Menu")
+async def prosmltor_Menu(message : types.Message):
+    await
+    cursors.execute(f'SELECT NamePiza FROM menu')
+
+
 if __name__ == '__main__':
-    executor.start_polling(skip_updates=True)
+    executor.start_polling(dis, skip_updates=True)
